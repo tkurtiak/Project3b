082d6dd0198aa485152a042e290d16d13b3e20c9
vdorbala <vdorbala@gmail.com>
2019-11-01 10:40:25 -0400

    Files for tag creation

--- tag.py
+++ tag.py
@@ -18,41 +18,37 @@ MARKERS_MAX = 100
 
 while not rospy.is_shutdown():
 
-   marker = Marker()
-   marker.header.frame_id = "/odom"
-   # marker.type = marker.SPHERE
-   # marker.action = marker.ADD
-   marker.scale.x = 1
-   marker.scale.y = 1
-   marker.scale.z = 1
-   marker.color.a = 0.0
-   marker.color.r = 1.0
-   marker.color.g = 1.0
-   marker.color.b = 1.0
-   marker.pose.orientation.w = 0.3
-   marker.pose.position.x = 0.8
-   marker.pose.position.y = 0.5
-   marker.pose.position.z = 0.6
+    marker = Marker()
+    marker.header.frame_id = "/odom"
+    marker.type = marker.MESH_RESOURCE
+    marker.mesh_resource = "package://ddmc/meshes/tag.dae"
+    marker.mesh_use_embedded_materials = True
+    # marker.action = marker.ADD
+    marker.scale.x = 1
+    marker.scale.y = 1
+    marker.scale.z = 1
+    # marker.color.a = 1.0
+    # marker.color.r = 1.0
+    # marker.color.g = 0.0
+    # marker.color.b = 0.0
+    marker.pose.orientation.w = 1
+    marker.pose.position.x = 1.5
+    marker.pose.position.y = -3.5
+    marker.pose.position.z = 0.0
 
-   marker.mesh_resource = "file://home/vdorbala/bebop_ws/src/ddmc/scripts/tagfinal.dae"
-   marker.mesh_use_embedded_materials = True
+    # We add the new marker to the MarkerArray, removing the oldest
+    # marker from it when necessary
+    # if(count > MARKERS_MAX):
+    #     markerArray.markers.pop(0)
 
-   # We add the new marker to the MarkerArray, removing the oldest
-   # marker from it when necessary
-   # if(count > MARKERS_MAX):
-   #     markerArray.markers.pop(0)
+    # markerArray.markers.append(marker)
 
-   # markerArray.markers.append(marker)
+    # # Renumber the marker IDs
+    # id = 0
+    # for m in markerArray.markers:
+    #     m.id = id
+    #     id += 1
 
-   # # Renumber the marker IDs
-   # id = 0
-   # for m in markerArray.markers:
-   #     m.id = id
-   #     id += 1
-
-   # Publish the MarkerArray
-   publisher.publish(marker)
-
-   count += 1
-
-   rospy.sleep(0.01)
+    # Publish the MarkerArray
+    publisher.publish(marker)
+    rospy.sleep(0.01)
